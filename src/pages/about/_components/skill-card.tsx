7af180941d0b6585eab8a6a7adbafbaeb0f839c6
Bùi Đức Nhật <nhaths4701@gmail.com>
2022-12-18 00:50:20 +0700

    Add technical skills

---
+++ src/pages/about/_components/skill-card.tsx
@@ -0,0 +1,23 @@
+import Link from '@docusaurus/Link';
+import React from 'react';
+import { usePalette } from 'react-palette';
+
+export default function SkillCard({ img, name, url }: { img: string; name: string; url: string }) {
+  const { data, loading, error } = usePalette(img);
+  console.log(data.vibrant);
+  return (
+    !!data.vibrant &&
+    !loading &&
+    !error && (
+      <Link
+        href={url}
+        className={`bg-white relative w-11 h-11 md:w-20 md:h-20 flex justify-center items-center m-1 rounded-md`}>
+        <div
+          style={{ backgroundColor: data.lightVibrant }}
+          className={`absolute top-0 opacity-30 hover:opacity-50 w-full h-full rounded-md`}
+        />
+        <img className="w-10 h-10 md:w-16 md:h-16" src={img} alt={name} />
+      </Link>
+    )
+  );
+}
--- src/pages/about/index.tsx
+++ src/pages/about/index.tsx
@@ -1,15 +1,38 @@
 import Translate from '@docusaurus/Translate';
 import Layout from '@theme/Layout';
 import React from 'react';
+import SkillCard from './_components/skill-card';
+
+import { technicalSkills } from './_skills-data';
 
 function AboutPage() {
+  console.log(technicalSkills);
+
   return (
     <Layout
       title="About"
       description="Personal website of Bùi Đức Nhật, a developer, sharing about programming, technology, and life.">
-      <h1>
-        <Translate>About me</Translate>
-      </h1>
+      <div className="min-h-[70vh] bg-gradient-to-br md:bg-gradient-to-tr from-primary-50 to-gray-50 dark:from-primary-900 dark:to-gray-900">
+        <div className="container py-5">
+          <h1 className="text-bold text-primary-900 dark:text-primary-50">
+            <Translate>About me</Translate>
+          </h1>
+
+          <div className="p-5 rounded-md">
+            <h2 className="text-center text-primary-800 dark:text-primary-100">
+              <Translate>Technical skills</Translate>
+            </h2>
+
+            <div className="w-full flex justify-center">
+              <div className="max-w-3xl flex flex-wrap justify-center">
+                {technicalSkills.map((x, index) => {
+                  return <SkillCard key={index} img={x.img} name={x.name} url={x.url} />;
+                })}
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </Layout>
   );
 }
