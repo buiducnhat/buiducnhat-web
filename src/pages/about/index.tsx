18d3e47180328e21f4261f41296237b3542e547d
Bùi Đức Nhật <nhaths4701@gmail.com>
2023-01-02 09:12:32 +0700

    Move skill to portfolio

--- src/pages/about/index.tsx
+++ src/pages/about/index.tsx
@@ -1,13 +1,8 @@
 import Translate from '@docusaurus/Translate';
 import Layout from '@theme/Layout';
 import React from 'react';
-import SkillCard from './_components/skill-card';
-
-import { technicalSkills } from './_skills-data';
 
 function AboutPage() {
-  console.log(technicalSkills);
-
   return (
     <Layout
       title="About"
@@ -17,20 +12,6 @@ function AboutPage() {
           <h1 className="text-bold text-primary-900 dark:text-primary-50">
             <Translate>About me</Translate>
           </h1>
-
-          <div className="p-5 rounded-md">
-            <h2 className="text-center text-primary-800 dark:text-primary-100">
-              <Translate>Technical skills</Translate>
-            </h2>
-
-            <div className="w-full flex justify-center">
-              <div className="max-w-3xl flex flex-wrap justify-center">
-                {technicalSkills.map((x, index) => {
-                  return <SkillCard key={index} img={x.img} name={x.name} url={x.url} />;
-                })}
-              </div>
-            </div>
-          </div>
         </div>
       </div>
     </Layout>
